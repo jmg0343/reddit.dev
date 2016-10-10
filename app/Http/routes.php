6c4a757ca3d4e27689c9451a07c59e66fba1299e
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-10 14:44:05 -0500

    route exercise

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,21 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// // below, will only work with defined $name
+// Route::get('/sayhello/{name}', function ($name) {
+// 	return 'Hello ' . $name;
+// });
+// // below, will work with no $name. Defaulted to 'Lassen'
+// Route::get('/sayhello/{name?}', function ($name = 'Lassen') {
+// 	return 'Hello ' . $name;
+// });
+// // below, will redirect anyone named "Chris" to home page
+// Route::get('/sayhello/{name}', function ($name) {
+// 	if ($name == "Chris") {
+// 		return redirect('/');
+// 	}
+// 	return 'Hello ' . $name;
+// });
+
+Route::get('/uppercase/{word}', function ($word) {
+	return strtoupper($word);
 });
-// below, will only work with defined $name
-Route::get('/sayhello/{name}', function ($name) {
-	return 'Hello ' . $name;
+
+Route::get('/increment/{number}', function($number) {
+	return ($number + 1);
 });
-// below, will work with no $name. Defaulted to 'Lassen'
-Route::get('/sayhello/{name?}', function ($name = 'Lassen') {
-	return 'Hello ' . $name;
-});
-// below, will redirect anyone named "Chris" to home page
-Route::get('/sayhello/{name}', function ($name) {
-	if ($name == "Chris") {
-		return redirect('/');
-	}
-	return 'Hello ' . $name;
+
+Route::get('/add/{a?}/{b?}', function($a = 2, $b = 2) {
+	return ($a + $b);
 });
